dee6181ea743ebb9ca83b4fad2d88fe3d6c57600
Revanth-Kariappa <revanthkariappa4@gmail.com>
2022-07-18 13:20:01 +0530

    sonarqube commit

--- src/test/java/com/LensCart/LensCartApplicationTests.java
+++ src/test/java/com/LensCart/LensCartApplicationTests.java
@@ -1,6 +1,9 @@
 package com.LensCart;
 
+
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
@@ -9,5 +12,7 @@ class LensCartApplicationTests {
 	@Test
 	void contextLoads() {
 	}
+
+
 
 }
